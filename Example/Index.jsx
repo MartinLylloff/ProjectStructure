9e07b89db4d068d73a7ea58a0d9c9c5d10bd7141
Martin <94110452+MartinLylloff@users.noreply.github.com>
2024-06-19 07:38:13 +0200

    Project Structure

---
+++ Example/Index.jsx
@@ -0,0 +1,13 @@
+import FirstComponent from "./FirstComponent";
+import SecondComponent from "./SecondComponent";
+
+const Example = () => {
+  return (
+    <>
+      <div>Example</div>
+      <FirstComponent />
+      <SecondComponent />
+    </>
+  );
+};
+export default Example;
